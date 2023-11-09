4cbae0ecee290ca533ec924af1f0a6796d925775
OctavianBirca <121047796+OctavianBirca@users.noreply.github.com>
2023-11-09 21:52:41 +0100

    HW done

---
+++ HW4-L12-say-yes-to-stop-OBirca.py
@@ -0,0 +1,12 @@
+#HW4-L12 - Say yes to stop - Octavian Birca
+
+
+while True:
+    print("BOO YA!")
+    answer = input("Do you want me to stop ? ")
+    
+    if answer == "yes":
+        print ("OK, OK!")
+        break
+
+
