abf1c535fa6c17b87c508caf5458abc095772480
OctavianBirca <121047796+OctavianBirca@users.noreply.github.com>
2023-11-09 22:42:53 +0100

    HW3-L12-Ship-Sea-Obirca.py
    "grand ship" replaced as a "mine"

--- HW3-L12-Ship-Sea-Obirca.py
+++ HW3-L12-Ship-Sea-Obirca.py
@@ -3,22 +3,22 @@
 
 big_ship = int(input('Enter the ship coordonates: '))
 
-grand_ship = 5
+mine = 5
 
 
 
 for x in range(1,11):
         
-    if x == big_ship and x != grand_ship: #big ship
+    if x == big_ship and x != mine: #big ship
       print( "W", end="" )
     
 #### the grand ship draw - start ####
-    elif x == grand_ship and x == big_ship:
+    elif x == mine and x == big_ship:
       print( "W", end="" )
-    elif x == grand_ship-1 and x == big_ship-1:
+    elif x == mine-1 and x == big_ship-1:
         print( "w", end="" )  
 
-    elif x == grand_ship+1 and x == big_ship+1:
+    elif x == mine+1 and x == big_ship+1:
         print( "w", end="" )  
 
 #### the grand ship draw - end ####
