ad35c4090c11e57be3a2b37f3549fa4b454d9078
OctavianBirca <121047796+OctavianBirca@users.noreply.github.com>
2023-11-19 13:03:22 +0100

    Add files via upload

---
+++ data-structure/HW5-dict-social-OBirca.py
@@ -0,0 +1,64 @@
+
+
+user = {
+
+    "username" : "Octavian",
+    "online"   : False,
+    "email"    : "octav@birca.com",
+    "rating"   : 165465321,
+    "friends"  : [
+        "Jean", 
+        "Fred",
+        "Pierre",
+    ]
+    
+}
+
+
+####### Add new friends #######
+new_friend = None
+
+while True:
+    print("To finish press ENTER")
+    new_friend = input("Add a new friend's name: ")
+    user["friends"].append(new_friend)
+
+    if new_friend == "":
+
+        break  # Exit the loop if input is empty
+
+
+####### show user status ######
+print()
+print("== USER STATUS ==")
+
+if user["online"] == True:
+    print ("🤨", user["username"], "✅")
+else:
+    print ("🤨", user["username"], "⛔")
+    
+print ("🖄 ", user["email"] )
+
+likes = None
+
+if user["rating"] == 0 or user["rating"] < 1000:
+    print ("💙", user["rating"] )
+
+elif user["rating"] == 1000 or user["rating"] < 1000000:
+    likes = user["rating"] // 1000
+    print ("💙", str(likes) + "M" )
+
+elif user["rating"] >= 1000000 :
+    likes = user["rating"] // 1000000
+    print ("💙", str(likes) + "T" )
+
+for f in range(len(user["friends"])-1):
+    print ("👫", user["friends"][f])
+
+print()
+
+
+
+
+
+  
