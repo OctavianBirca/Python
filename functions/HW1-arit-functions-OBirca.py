db790d2f1c18b97108bccf2d7ea3c8f4f669b624
OctavianBirca <121047796+OctavianBirca@users.noreply.github.com>
2023-11-25 11:55:06 +0100

    Create HW1-arit-functions-OBirca.py

---
+++ functions/HW1-arit-functions-OBirca.py
@@ -0,0 +1,25 @@
+# arithmetical functions
+
+# Addition
+def add( a, b, ):
+    return a + b
+# Subtraction
+def sub( a, b, ):
+    return a - b
+
+# Multiplication
+def mul( a, b, ):
+    return a * b
+
+# Division
+def div( a, b, ):
+    return a / b
+
+
+r = 1 + 2 * 3 / 4
+R = add(div(mul(2, 3), 4), 1)
+
+
+print(r) # control
+
+print(R)
